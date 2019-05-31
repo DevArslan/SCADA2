4cdc2ff20e937d8c238db8558d9b251faceeb127
DevArslan <arslan.gareev.2017@gmail.com>
2019-06-01 01:30:56 +0300

    Начало создания страницы "База данных". Выводит данные из таблицы в PostgreSQL.

--- database/views.py
+++ database/views.py
@@ -1,8 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import psycopg2, random
+import json
+
 
 def database(request):
-	return (render(request, 'database.html'))
 
-# функция render возвращается html страницу
+	con = None
+	con = psycopg2.connect("host='localhost' dbname='postgres' user='postgres' password='Direling2017'")
+	cur = con.cursor()
+	cur.execute("SELECT * FROM DATA_TABLE")
+	data = cur.fetchall()
+
+	number = [x[0] for x in data]
+	date = [x[1] for x in data]
+	name = [x[2] for x in data]
+	size = [x[3] for x in data]
+
+	cur.close()
+	con.close()
+
+
+	return (render(request, 'database.html',{'number_array': number,'date_array': date,'name_array': name,'size_array': size}))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
